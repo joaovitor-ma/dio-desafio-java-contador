a3e46a00ec6d9c0d2c54def54d4fd4098905f1d4
João Vitor <jvitorcavalcante18@gmail.com>
2025-06-10 19:02:50 -0300

    Instanciando scanner, solicitando parametros e realizando a contagem

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -1,5 +1,41 @@
+import java.util.Scanner;
+
+import exceptions.ParametrosInvalidosException;
+
 public class Contador {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello World!");
+        
+        // Instanciando Scanner
+        Scanner terminal = new Scanner(System.in);
+
+        int paramUm;
+        int paramDois;
+
+        // Solicitando parametros e atribuindo às suas variáveis
+        System.out.println("Insira o primeiro parametro");
+        paramUm = terminal.nextInt();
+        System.out.println("Insira o segundo parametro");
+        paramDois = terminal.nextInt();
+
+        contar(paramUm, paramDois);
+
+        terminal.close();
+    }
+
+    private static void contar(int paramUm, int paramDois){
+
+        // Checando parametros e lançando exceção
+        if (paramUm > paramDois) {
+            throw new ParametrosInvalidosException("O primeiro parametro não pode ser menor que o segundo");
+        }
+
+        // Definindo limite do contador
+        int contador = paramDois - paramUm;
+
+        // Realizando a contagem
+        for(int i = 1; i <= contador; i++){
+            System.out.println("Imprimindo o número "+i);
+        }
+
     }
 }
